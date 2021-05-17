0403ba030ce22b86bc60b0ee88254298e8872407
Ondrej Čupaj <76491111+onocpj@users.noreply.github.com>
2021-05-17 16:23:07 +0200

    Update ThreadPool.cpp

--- StraveEngine/src/System/Thread/ThreadPool.cpp
+++ StraveEngine/src/System/Thread/ThreadPool.cpp
@@ -1,8 +1,10 @@
-#include "ThreadPool.hpp"
-#include "Timer.hpp"
+#include <StraveEngine/System/Thread/ThreadPool.hpp>
 
-#include <iostream>
-#include <string>
+#include <StraveEngine/System/Thread/Thread.hpp>
+#include <StraveEngine/System/Exception.hpp>
+#include <StraveEngine/System/Application.hpp>
+#include <StraveEngine/System/Log.hpp>
+#include <StraveEngine/System/MapContainer.cpp>
 
 
 namespace Strave
@@ -24,21 +26,43 @@ namespace Strave
 		ThreadPool::m_ThreadPool->StartThreads();
 	}
 
-	/*Exception*/ int ThreadPool::Delete(void)
+	Exception ThreadPool::Delete(void)
 	{
-		// try
-		// {
-		// 	delete ThreadPool::m_ThreadPool;
-		// 	ThreadPool::m_ThreadPool = nullptr;
-		// }
-		// catch (...)
-		// {
-		// 	return THROW__EXC_THRP_DEL;
-		// }
-		// 
-		// return NO_EXCEPTION;
+		Uint16 freedUpThreadsNum = UNDEF_UINT16;
+		ThreadPool* instance = ThreadPool::Get();
 
-		return 0;
+		if (instance != UNDEF_PTR)
+		{
+			try 
+			{
+				for (Uint16 index = 0; index != ThreadPool::Threads::Count; index++) 
+				{
+					if (instance->m_ThreadContainer.at(index)->Joinable()) 
+					{
+						instance->m_ThreadContainer.at(index)->Join();
+						delete instance->m_ThreadContainer.at(index);
+						instance->m_ThreadContainer.at(index) = nullptr;
+
+						freedUpThreadsNum++;					
+					}
+				}
+
+				if (freedUpThreadsNum == ThreadPool::Threads::Count)
+				{
+					delete ThreadPool::m_ThreadPool;
+					ThreadPool::m_ThreadPool = nullptr;
+				}
+				else 
+					throw Exception();
+
+			}
+			catch (...) 
+			{
+				return THROW__EXC_THRP_DEL;
+			}
+		}
+
+		return NO_EXCEPTION;
 	}
 
 	Int16 ThreadPool::GetThreadStatus(const std::thread::id& threadID)
@@ -51,197 +75,163 @@ namespace Strave
 		return THRP_UNDEF_THREAD_STATE;
 	}
 
-	Uint16 ThreadPool::Enqueue(Task::NoParam task)
-	{
-		EnqueuedTask* enqTask = new EnqueuedTask {
-			std::move(task),
-			!THRP_TASK_LOCKED,
-			!THRP_TASK_COMPLETED,
-			Task::Priority::Low,
-			THRP_UNDEF_THREAD_OWNER
-		};
-
-		std::queue<ThreadPool::EnqueuedTask*>* selectedQueue = &GetCorrectQueue(*enqTask);
-		selectedQueue->push(enqTask);
-
-		return UNDEF_UINT16; // should return thread key
-	}
-
-	void ThreadPool::EnqueueParallel(Task::NoParam task)
-	{
-		EnqueuedTask* enqTask = new EnqueuedTask{
-			std::move(task),
-			!THRP_TASK_LOCKED,
-			!THRP_TASK_COMPLETED,
-			Task::Priority::Low,
-			THRP_UNDEF_THREAD_OWNER
-		};
-
-		std::queue<ThreadPool::EnqueuedTask*>* selectedQueue = &GetCorrectQueue(*enqTask);
-		selectedQueue->push(enqTask);
-	}
-
 	void ThreadPool::Mute(Task::NoParam& task)
 	{
 		std::mutex mutex;
-		std::unique_lock<std::mutex> locker{ mutex };
+		std::unique_lock<std::mutex> locker(mutex);
 
 		task();
 
 		locker.unlock();
 	}
 
-	void ThreadPool::Lock(Uint16 thread)
+	Int16* ThreadPool::Enqueue(Task::NoParam task)
+	{
+		EnqueuedTask* enqTask = new EnqueuedTask{
+			THRP_KEY_UNASSIGNED,
+			THRP_OWNER_UNASSIGNED,
+			std::move(task),
+			!THRP_TASK_LOCKED,
+			!THRP_TASK_COMPLETED,
+			Task::Priority::Low,
+			new std::mutex()
+		};
+
+		ThreadPool::Get()->m_TaskQueue.push_back(enqTask);
+
+		return &enqTask->Owner; // should return thread key
+	}
+
+	void ThreadPool::EnqueueParallel(Task::NoParam task)
+	{
+		EnqueuedTask* enqTask = new EnqueuedTask{
+			THRP_KEY_UNASSIGNED,
+			THRP_OWNER_UNASSIGNED,
+			std::move(task),
+			!THRP_TASK_LOCKED,
+			!THRP_TASK_COMPLETED,
+			Task::Priority::Low,
+			new std::mutex()
+		};
+
+		ThreadPool::Get()->m_TaskQueue.push_back(enqTask);
+	}
+
+	void ThreadPool::Lock(Int16* thread)
 	{
 		return;
 	}
 
-	void ThreadPool::Unlock(Uint16 thread)
+	void ThreadPool::Unlock(Int16* thread)
 	{
 		return;
 	}
 
-	void ThreadPool::Wait(Uint16 thread)
+	void ThreadPool::Wait(Int16* thread)
 	{
+		// auto predicate = [&thread]() -> bool {
+		// 	return (thread != nullptr && *thread >= 0);
+		// };
+		// 
+		// while (!predicate())
+		// {}
+
+		{
+			std::condition_variable threadProceededTask;
+			std::mutex waitThread;
+
+			std::unique_lock<std::mutex> taskProceededLock(waitThread);
+			threadProceededTask.wait(
+				taskProceededLock,
+				THRP_PREDICATE(return (thread != nullptr && *thread >= 0))
+			);
+		}
+
+		{
+			std::unique_lock<std::mutex> taskStateLock(*(ThreadPool::Get()->m_ThreadContainer[*thread]->TaskHolder->TaskStateMutex));
+			ThreadPool::Get()->m_ThreadContainer[*thread]->CV->wait(
+				taskStateLock,
+				THRP_PREDICATE(return ThreadPool::Get()->m_ThreadContainer[*thread]->TaskHolder->Completed)
+			);
+		}
+
+		EnqueuedTask* taskToTerminate = ThreadPool::Get()->m_TerminateTaskMap.GetElement((Uint64)ThreadPool::Get()->m_ThreadContainer[*thread]->TaskHolder->KEY);
+		ThreadPool::Get()->m_TerminateTaskMap.EraseFromContainer((Uint64)ThreadPool::Get()->m_ThreadContainer[*thread]->TaskHolder->KEY);
+
+		delete taskToTerminate;
+		taskToTerminate = nullptr;
+
 		return;
 	}
 
 	void ThreadPool::StartThreads(void)
 	{
-		Thread::Type basicTypeBuffer;
-		Thread::GameType gameTypeBuffer = Thread::GameType::RenderThread;
-		Thread::ThreadType threadTypeBuffer;
-
 		for (Uint16 index = 0; index < ThreadPool::Threads::Count; index++)
 		{
-			basicTypeBuffer = GetThreadType(ThreadPool::Threads::Count, index);
-			gameTypeBuffer = GetThreadGameType(index);
-
-			threadTypeBuffer = {
-				gameTypeBuffer,
-				basicTypeBuffer
-			};
-
 			auto threadWorker = std::move(
 				LAMBDAV_START
 
-					Uint16 threadKey = index;
-					Thread::ThreadType threadType = threadTypeBuffer;
-					while (true /* !StraveApplication->GetOnExitState() */) { ProceedTask(threadKey, threadType); } 
+				Uint16 threadID = index;
+			while (true /* !StraveApplication->GetOnExitState() */) { ProceedTask(threadID); }
 
-				LAMBDA_END
-			);
+			LAMBDA_END
+				);
 
-			std::mutex* threadMutex = new std::mutex(); // each thread needs to have own mutex for task state
-			Thread* thread = new Thread(threadTypeBuffer, threadWorker, Thread::ThreadType::ToString(threadTypeBuffer));
-			ThreadPool::Get()->m_ThreadContainer.push_back(std::make_shared<ThreadPack>(thread, threadMutex)); 
+			Thread* thread = new Thread(threadWorker);
+			ThreadPool::Get()->m_ThreadContainer.push_back(std::make_shared<ThreadPack>(thread));
 		}
 	}
 
-	void ThreadPool::ProceedTask(Uint16 thread, Thread::ThreadType& threadType)
+	void ThreadPool::ProceedTask(Uint16 thread)
 	{
-		std::queue<ThreadPool::EnqueuedTask*>* selectedQueue = &GetCorrectQueue(threadType);
 		std::unique_lock<std::mutex> taskQueueLock(m_TaskQueueMutex);
 
-		if (!selectedQueue->empty())
+		if (!m_TaskQueue.empty())
 		{
-			Timer Timer("Proceeding task");
+			// Timer Timer("Proceeding task");
 
-			EnqueuedTask* enqTask = selectedQueue->front();
-			enqTask->ThreadOwner = thread;
-			Task::NoParam task = std::move(enqTask->_Task.Get());
+			EnqueuedTask* enqTask = m_TaskQueue.front();
+			m_ThreadContainer[thread]->TaskHolder = enqTask;
+			m_ThreadContainer[thread]->ExecutableTask = enqTask->_Task.Get();
+			m_TaskQueue.pop_front();
 
-			selectedQueue->pop();
 			taskQueueLock.unlock();
+			m_ThreadContainer[thread]->ExecutableTask();
 
-			// ThreadFlag threadRise(*this, m_ThreadContainer.at(index));	
-			task();
+			{
+				std::unique_lock<std::mutex> taskStateLock(*(enqTask->TaskStateMutex));
+				enqTask->Completed = THRP_TASK_COMPLETED;
+				enqTask->Owner = thread;
 
-			// Use thread mutex and change task state to completed
-			std::unique_lock<std::mutex> taskStateLock(*(m_ThreadContainer[thread]->TaskStateMutex));
-			enqTask->Completed = true;
-			taskStateLock.unlock();
+				enqTask->KEY = (Uint16)m_TerminateTaskMap.InsertToContainerRecycled(*enqTask);
+			}
 
-			delete enqTask;
-			enqTask = nullptr;
+			m_ThreadContainer[thread]->CV->notify_one();
 		}
 	}
 
-	std::queue<ThreadPool::EnqueuedTask*>& ThreadPool::GetCorrectQueue(Thread::ThreadType& threadType)
-	{
-		if (threadType._Type == Thread::Type::Worker)
-			if (!m_TaskQueue.WorkerQueue.empty())
-				return m_TaskQueue.WorkerQueue;
-
-		return m_TaskQueue.GetwayQueue;
-	}
-
-	std::queue<ThreadPool::EnqueuedTask*>& ThreadPool::GetCorrectQueue(EnqueuedTask& enqTask)
-	{
-		if (enqTask.Locked)
-			return ThreadPool::Get()->m_TaskQueue.WorkerQueue;
-
-		return ThreadPool::Get()->m_TaskQueue.GetwayQueue;
-	}
-
 	////////////////////////////////////////////////////////////
-	/// ThreadPack
+	/// EnqueuedTask
 	////////////////////////////////////////////////////////////
-	ThreadPool::ThreadPack::ThreadPack(Thread* thread, std::mutex* mutex) :
-		_Thread(thread),
-		TaskStateMutex(mutex),
-		SelectedQueue(UNDEF_PTR)
-	{}
-
-	ThreadPool::ThreadPack::~ThreadPack()
+	ThreadPool::EnqueuedTask::~EnqueuedTask()
 	{
 		delete TaskStateMutex;
 		TaskStateMutex = nullptr;
 	}
 
 	////////////////////////////////////////////////////////////
-	/// Functions
+	/// EnqueuedTask
 	////////////////////////////////////////////////////////////
-	Uint16 ChooseOptimalGetwayNumber(Uint16 totalThreads)
+	ThreadPool::ThreadPack::ThreadPack(Thread* thread) :
+		_Thread(thread),
+		TaskHolder(THRP_EMPTY_TASK_HOLDER),
+		ExecutableTask(THRP_EMPTY_EXC_TASK),
+		CV(new std::condition_variable())
+	{}
+
+	ThreadPool::ThreadPack::~ThreadPack()
 	{
-		Uint16 getwayCounter = UNDEF_INT16;
-
-		// if total number of threads is more than 3
-		if (totalThreads > 3)
-		{
-			// create more getways
-			if (totalThreads % 2 != 0)
-				getwayCounter++;
-
-			for (Uint16 i = 2; i <= totalThreads; i++)
-			{
-				if (totalThreads % i == 0)
-					getwayCounter++;
-			}
-		}
-		else
-			getwayCounter = 1; // else create only one getway
-
-		return getwayCounter;
-	}
-
-	Thread::Type GetThreadType(Uint16 totalThreads, Uint16 currIterartion)
-	{
-		Uint16 getwayNumber = ChooseOptimalGetwayNumber(totalThreads);
-		return currIterartion < (totalThreads - getwayNumber) ? Thread::Type::Worker : Thread::Type::Getway;
-	}
-
-	Thread::GameType GetThreadGameType(Uint16 currIterartion)
-	{
-		if(currIterartion <= 2)
-			// we need to add 2 to current iteration because rendering thread is main thread, 
-			// so we dont want to initialize any of the additional threads as rendering thread.
-			// also we need to skip uninitialized thread type 
-			//
-			// +1 : for undefined thread
-			// +1 : for rendering thread
-			return static_cast<Thread::GameType>(currIterartion + 2);
-		else
-			return Thread::GameType::Undefined;
+		delete CV;
+		CV = nullptr;
 	}
 }
